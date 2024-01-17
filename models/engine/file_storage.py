6d42edc78160959a48310ec06b920d69394d05dc
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-17 20:25:22 +0000

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,22 +8,25 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Returns a dictionary or a list of models currently in storage"""
+        if cls is not None:
+            return {k: v for k, v in FileStorage.__objects.items() if isinstance(v, cls)}
         return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = obj.__class__.__name__ + '.' + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            for key, val in FileStorage.__objects.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(serialized_objects, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -40,17 +43,21 @@ class FileStorage:
             'State': State, 'City': City, 'Amenity': Amenity,
             'Review': Review
         }
+
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    FileStorage.__objects[key] = classes[val['__class__']](**val)
+                serialized_objects = json.load(f)
+                for key, serialized_obj in serialized_objects.items():
+                    class_name = serialized_obj.pop('__class__', None)
+                    if class_name in classes:
+                        obj = classes[class_name](**serialized_obj)
+                        FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes obj from __objects if it's inside"""
+        """Deletes an object from __objects if it's inside"""
         if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            FileStorage.__objects.pop(key, None)
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
