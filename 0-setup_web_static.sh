f72487922613300e55f03b8802543d765b97f00a
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-01-31 15:03:29 +0000

    fast_push

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Prepare the web servers
+
+sudo apt-get update
+sudo apt-get install nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -hR ubuntu:ubuntu /data/
+
+sudo sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
