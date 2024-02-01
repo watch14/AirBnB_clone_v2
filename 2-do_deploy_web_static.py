55ccbcc54f6896baf2a9554f553bef40ba0e1d5b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-01 22:03:12 +0000

    fast_push

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,33 +7,34 @@ env.hosts = ["54.160.85.72", "35.175.132.106"]
 
 
 def do_deploy(archive_path):
-    """ deploy """
-    if not os.path.isfile(archive_path):
+    """Distributes an archive to the web server"""
+    if os.path.isfile(archive_path) is False:
         return False
-
-    file = os.path.basename(archive_path)
+    file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, f"/tmp/{file}").failed:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run(f"tar -xzf /tmp/{file} -C /\
-            data/web_static/releases/{name}/").failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-    if run(f"rm /tmp/{file}").failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run(f"mv /data/web_static/releases/{name}/web_static/*\
-            /data/web_static/releases/{name}/").failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/current").failed:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run(f"ln -s /data/web_static/releases/{name}/\
-            /data/web_static/current").failed:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
-
     return True
