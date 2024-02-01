be4303d420672b4c10761c1ecf028d15daea521f
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-01 14:09:27 +0000

    fast_push

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
-""" upload web_static / uncompres / create new symbolik link """
+# Distribute an archive to a web server
+import os.path
+from fabric.api import env, put, run
 
-from fabric.api import *
-from datetime import datetime
-from os.path import exists
-
-env.hosts = ['52.91.147.124', '107.21.183.54']
+env.hosts = ["54.160.85.72", "35.175.132.106"]
 
 
 def do_deploy(archive_path):
     """ deploy """
-
-    if exists(archive_path) is False:
+    if not os.path.isfile(archive_path):
         return False
 
-    filename = archive_path.split('/')[-1]
-    no_tgz = f'/data/web_static/releases/{filename.split(".")[0]}'
-    tmp = f'/tmp/{filename}'
+    file = os.path.basename(archive_path)
+    name = file.split(".")[0]
 
-    # Upload to tmp
-    put_result = put(archive_path, '/tmp/')
-    if put_result.failed:
+    if put(archive_path, f"/tmp/{file}").failed:
         return False
-
-    # Uncompress /data/web_static/releases
-    run_result1 = run(f'mkdir -p {no_tgz}/')
-    run_result2 = run(f'tar -xzf {tmp} -C {no_tgz}/')
-    run_result3 = run(f'rm {tmp}')
-
-    # Move to web static / delete old files
-    run_result4 = run(f'mv {no_tgz}/web_static/* {no_tgz}/')
-    run_result5 = run(f'rm -rf {no_tgz}/web_static')
-
-    # create a new symbolic link
-    run_result6 = run('rm -rf /data/web_static/current')
-    run_result7 = run(f'ln -s {no_tgz}/ /data/web_static/current')
-
-    if (put_result.failed or run_result1.failed or run_result2.failed or
-            run_result3.failed or run_result4.failed or run_result5.failed or
-            run_result6.failed or run_result7.failed):
+    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"tar -xzf /tmp/{file} -C /\
+            data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"rm /tmp/{file}").failed:
+        return False
+    if run(f"mv /data/web_static/releases/{name}/web_static/*\
+            /data/web_static/releases/{name}/").failed:
+        return False
+    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
+        return False
+    if run(f"rm -rf /data/web_static/current").failed:
+        return False
+    if run(f"ln -s /data/web_static/releases/{name}/\
+            /data/web_static/current").failed:
         return False
 
     return True
