3e4ac6935a9614ac02cd85323eba17254f6861cd
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-01 23:20:22 +0000

    fast_push

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -20,9 +20,9 @@ def do_pack():
                                                          dt.minute,
                                                          dt.second)
     if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+        if local("sudo mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("sudo tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
 
