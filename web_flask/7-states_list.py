9068e21d46bfcb48a94e1893f40073e77f9a2b8b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-17 08:37:12 +0000

    fast_push

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,72 +1,64 @@
 #!/usr/bin/python3
-""" flask 2 """
+""" module doc """
 from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def index():
-    """ fuinc """
+def hello():
+    """ def doc """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def indextwo():
-    """ func2 """
+def hbnb():
+    """ def doc """
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def indexthree(text):
-    """ func3 """
-    text = text.replace("_", " ")
-    return "C " + f"{text}"
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def indexfour(text):
-    """ func4 """
+def python(text):
+    """ def doc """
     return 'Python {}'.format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def indexfive(n):
-    """ func5 """
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def indexsix(n):
-    """ func6 """
-    return render_template('5-number.html', number=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def indexseven(n):
-    """ func7 """
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
     if n % 2 == 0:
-        p = "even"
+        p = 'even'
     else:
-        p = "odd"
-    return render_template('6-number_odd_or_even.html', number=n, x=p)
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ func list """
+    """ def doc """
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def close(error):
-    """ fnc err """
+    """ def doc """
     storage.close()
 
 
