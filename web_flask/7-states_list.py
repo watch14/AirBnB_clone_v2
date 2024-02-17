46cd375db20bac9fef0b7d26c66ca3cb21c968c9
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-17 08:59:04 +0000

    fast_push

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-""" flask 2 """
-from flask import Flask
-from flask import render_template
+"""start app"""
+from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ func list """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def close(error):
-    """ fnc err """
+def handle_teardown(self):
+    """teardow"""
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """ list sates """
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
