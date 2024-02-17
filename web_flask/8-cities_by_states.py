c2223a78282d97f3215e5d00b84621688369a8e6
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-17 09:15:36 +0000

    fast_push

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" falsk cities """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ tear it down """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cityandstate_list():
+    """ list cities / states """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
