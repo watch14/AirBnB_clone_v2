6717355ee9b96d75612afedc0687f1b2e0399ac1
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-17 07:00:54 +0000

    fast_push

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" flask 2 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ fuinc """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def indextwo():
+    """ func2 """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def indexthree(text):
+    """ func3 """
+    text = text.replace("_", " ")
+    return "C " +f"{text}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
