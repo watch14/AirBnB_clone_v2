96c56f1f8e2c286254dd45112d50c68f2b3cf65b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-17 07:08:24 +0000

    fast_push

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" flask 2 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ fuinc """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def indextwo():
+    """ func2 """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def indexthree(text):
+    """ func3 """
+    text = text.replace("_", " ")
+    return "C " + f"{text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def indexfour(text):
+    """ func4 """
+    text = text.replace("_", " ")
+    return "python " + f"{text}"
+
+
+@app.route("/python", strict_slashes=False)
+def indexfive():
+    """ func5 """
+    return "python is cool"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
